dd2dd5b75aa3feb0ea5813d986bcfdf51015f3a7
Fabian M <mail.fabianm@gmail.com>
2015-10-23 21:51:58 +0200

    Remove useless utilities

--- DEV207.1x/lesson-3.fs
+++ DEV207.1x/lesson-3.fs
@@ -4,18 +4,13 @@ open System
 // Golden ratio
 let phi = (1.0 + Math.Sqrt(5.0)) / 2.0
 
-// Utilities
-let dup x = (x, x)
-let map f (a, b) = (f a, f b)
-let mapRight f (a, b) = (a, f b)
-
 [<EntryPoint>] 
 let main argv =
             printfn "Enter your numbers separated by newlines. To end input, enter a blank line."
             fun _ -> Console.ReadLine()
             |> Seq.initInfinite
             |> Seq.takeWhile ((<>) "") 
-            |> Seq.map (dup >> map float >> mapRight ((*) phi))
+            |> Seq.map (fun x -> (x, x * phi))
             |> Seq.toList
             |> printfn "%A"
             0
