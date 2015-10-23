5195ab9bb24acfa2a46f2ac975e8fbbf5696deab
Fabian M <mail.fabianm@gmail.com>
2015-10-23 22:17:23 +0200

    Read one line instead of multiple

--- DEV207.1x/lesson-3.fs
+++ DEV207.1x/lesson-3.fs
@@ -6,11 +6,10 @@ let phi = (1.0 + Math.Sqrt(5.0)) / 2.0
 
 [<EntryPoint>] 
 let main argv =
-            printfn "Enter your numbers separated by newlines. To end input, enter a blank line."
-            fun _ -> Console.ReadLine()
-            |> Seq.initInfinite
-            |> Seq.takeWhile ((<>) "") 
-            |> Seq.map (fun x -> (x, x * phi))
+            printf "Enter numbers separated by spaces: "
+            Console.ReadLine().Split(' ')
+            |> Seq.filter (Double.TryParse >> fst)
+            |> Seq.map (float >> fun x -> (x, x * phi))
             |> Seq.toList
             |> printfn "%A"
             0
