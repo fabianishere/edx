5c0a7e6495bae689241e1f2e018530a061ee01db
Fabian M <mail.fabianm@gmail.com>
2015-11-02 23:30:54 +0100

    Use active patterns

--- DEV207.1x/lesson-4/problem.fs
+++ DEV207.1x/lesson-4/problem.fs
@@ -7,24 +7,27 @@ type Shot = {
     distance : float;
     name : string 
 }
-    
+
+let g = 9.81
+let angle x y = atan (y / x)
+let angleOfReach distance speed = 0.5 * asin((g * distance) / (pown speed 2))
+let distance speed angle = (sin 2.0 * angle) * (pown speed 2) / g
+
+let (|Hit|_|) shot = if abs(shot.angle - (angleOfReach shot.distance shot.speed)) < 0.00000001 then Some() else None
+let (|Miss|_|) shot = 
+    match angleOfReach shot.distance shot.speed with
+    | angle when abs(shot.angle - angle) <  0.00000001 -> Some(1.0)
+    | angle when Double.IsNaN angle -> None
+    | angle -> Some(angle)
+
 let ParseShot (values : string array) = { 
-    angle = atan(float(values.[1]) / float(values.[0]));
-    speed = float(values.[2]); 
-    distance = float(values.[3]); 
+    angle = angle (float values.[0]) (float values.[1]);
+    speed = float values.[2]; 
+    distance = float values.[3]; 
     name = values.[4] 
 }
 
 let ProcessFile path = File.ReadLines(path) |> Seq.map (fun line -> line.Split(',')) |> Seq.map ParseShot
-
-let AngleOfReach shot = (shot, 0.5 * asin((9.81 * shot.distance) / (shot.speed * shot.speed)))
-
-let Result (shot, angle) = 
-    match angle with
-    | angle when Double.IsNaN angle -> sprintf "%s: impossible to hit this shot" shot.name
-    | _ when shot.angle = nan -> sprintf "%s: invalid angle for shot" shot.name
-    | angle when abs(shot.angle - angle) < 0.00000001 -> sprintf "%s: hit" shot.name
-    | _ -> sprintf "%s: miss (angle of %f required degrees; %f degrees given)" shot.name (angle * 180.0/Math.PI) (shot.angle * 180.0/Math.PI)
 
 [<EntryPoint>]    
 let main argv = 
@@ -32,10 +35,12 @@ let main argv =
     | 0 -> printfn "usage: ./program file1 file2 ..."; 0
     | _ ->
         try
-            Seq.ofArray argv
-            |> Seq.map (ProcessFile >> Seq.map (AngleOfReach >> Result))
-            |> Seq.concat
-            |> Seq.iter (printfn "%s")
+            let shots = Seq.ofArray argv |> Seq.map ProcessFile |> Seq.concat
+            for shot in shots do
+                match shot with
+                | Hit -> printfn "%s hits!" shot.name
+                | Miss(angle) -> printfn "%s misses. Angle of %f degrees required." shot.name (angle * 180.0 / Math.PI)
+                | _ -> printfn "%s is an impossible shot." shot.name
             0
         with
         | :? System.IO.FileNotFoundException ->
